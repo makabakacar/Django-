7239f72b27996c3a27568015b541ee89b3aec75c
lilily <lilily_li@163.com>
2020-12-07 19:22:06 +0800

    保存数据

--- web_demo/users/models.py
+++ web_demo/users/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length=20, verbose_name="用户名")
+    password = models.CharField(max_length=128, verbose_name='密码')
+    # default：设置向数据库添加数据时，字段使用的默认值
+    gender = models.BooleanField(default=False, verbose_name='性别')
+    age = models.IntegerField(default=18, verbose_name='年龄')
+    # null=True：生成数据表时，数据表的对应字段允许为NULL
+    mobile = models.CharField(max_length=11, null=True, verbose_name='手机号')
+
+    class Meta:
+        db_table = "tb_users"
--- web_demo/users/views.py
+++ web_demo/users/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from users.models import User
 # Create your views here.
 def register(request):
-    return render(request, "register.html")
+    if request.method == "GET":
+        return render(request, "register.html")
+    else:
+
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        # print(username, password)
+        user = User.objects.create(username=username, password=password)
+        return HttpResponse("进行注册业务处理")
