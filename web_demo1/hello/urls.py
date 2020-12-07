c454a023be5f2b76565bca7e5a09ce2083e90920
lilily <lilily_li@163.com>
2020-12-07 21:21:34 +0800

    django 的第二次练习

--- web_demo1/hello/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import re_path
-from hello import views
-
-urlpatterns = [
-    re_path(r'^hello/$', views.first_editor),
-]
--- web_demo1/user/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import re_path
-from user import views
-
-urlpatterns = [
-    re_path(r'^register/$', views.register),
-]
